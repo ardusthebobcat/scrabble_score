af249e9b112ce237bd6c97c517a88affd6d40efd
Michael Summers & George Denkey <student@epicodus.com>
2015-08-04 12:12:41 -0700

    Added a longer word on spec, tested it, was successful

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -10,4 +10,7 @@ describe("String#scrabble_score") do
   it("returns a scrabble score for an entire word") do
     expect("cat".scrabble_score()).to(eq(5))
   end
+  it("returns a scrabble score for a longer word") do
+    expect("jerry".scrabble_score()).to(eq(15))
+  end
 end
